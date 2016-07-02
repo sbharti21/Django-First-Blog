41f05cca9c2f0e3cd17115e14c3b5e1103858d78
Redeye User <redeyeuser@Thams-MacBook-Pro.local>
2016-07-02 15:39:06 +1000

    removed space

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,4 @@ from .models import Post
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    print(posts)
     return render(request, 'blog/post_list.html', {'posts': posts})
